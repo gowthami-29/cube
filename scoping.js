042e65c535b8c1a5ffe37e2a6c5182102dd4aeef
gowthami-29 <gowthamikanchi29@gmail.com>
2025-11-22 22:49:40 +0530

    scoping

---
+++ scoping.js
@@ -0,0 +1,19 @@
+if (true) {
+  let x = 10;
+  var y = 20;
+}
+console.log(y);
+console.log(x);
+
+
+
+const profile = {
+  user: {
+    details: {
+      email: "test@mail.com"
+    }
+  }
+};
+
+
+
