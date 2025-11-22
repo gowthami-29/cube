46e82306f39cd3a3e7b9029ef9ab97d3dd1508ed
gowthami-29 <gowthamikanchi29@gmail.com>
2025-11-22 21:59:07 +0530

    temperal

---
+++ temperal.js
@@ -0,0 +1,18 @@
+let username="gowthami";
+let course="masai"
+console.log(`Hello ${username},Welcome to the ${course} course!`)
+
+
+
+const name ="sam";
+const age =21;
+const student ={
+    name,
+    age,
+    greet()
+    {
+        console.log("Hello");
+    }
+}
+
+const getFullName=(first,last)=> `${first} ${last}`
